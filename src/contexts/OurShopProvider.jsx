5d047e995bee7d491f150ac3b58424c29c43688a
phuocly789 <phuoclyminh789@gmai.com>
2025-05-25 22:05:26 +0700

    fixcode

--- src/contexts/OurShopProvider.jsx
+++ src/contexts/OurShopProvider.jsx
@@ -6,9 +6,9 @@ export const OurShopContext = createContext();
 export const OurShopProvider = ({ children }) => {
     const sortOptions = [
         { label: 'Default sorting', value: '0' },
-        { label: 'Sort by popularity', value: '1' },
-        { label: 'Sort by average rating', value: '2' },
-        { label: 'Sort by latest', value: '3' },
+        // { label: 'Sort by popularity', value: '1' },
+        // { label: 'Sort by average rating', value: '2' },
+        // { label: 'Sort by latest', value: '3' },
         { label: 'Sort by price: low to high', value: '4' },
         { label: 'Sort by price: high to low', value: '5' },
     ];
